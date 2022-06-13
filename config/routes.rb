ac7cece8b76ad13179f9e4de36c0b1de2420f57a
Gegardus <stgegard@gmail.com>
2022-06-13 22:47:05 +0400

    Create an API endpoint to select a random greeting from table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :v1, default: { format: 'json'} do
+    get 'greetings' => 'greetings#index'
+  end
 
-  root "static#index"
+  root 'static#index'
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
