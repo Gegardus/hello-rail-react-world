deb23cdffedd72e605eb6aac69a28e28f6ba1fff
Gegardus <stgegard@gmail.com>
2022-06-14 03:31:27 +0400

    Add home page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { Provider } from 'react-redux';
 import {  BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
+import Home from './Home';
 import store from '../redux/configureStore';
 
 const App = () => {
   return (
     <Provider store={store}>
     <BrowserRouter>  
-    <Routes> 
+    <Routes>
+    <Route
+          path="/"
+          element={(
+            <Home />
+        )}
+        />
     <Route
           path="/greeting"
           element={(
